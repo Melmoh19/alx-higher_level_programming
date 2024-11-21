ac321c1906e95a574e2dddcf2263985ec3bf0d35
Melmoh19 <andersonhart003@gmail.com>
2024-11-21 03:15:37 +0300

    0-privileges.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,9 @@
--- Create table, unique_id
-CREATE TABLE IF NOT EXISTS unique_id (
-       id INT UNIQUE DEFAULT 1,
-       name VARCHAR(256)
+-- Create database, hbtn_0d_usa, and table states (in database, hbtn_0d_usa)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+       id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
+       name VARCHAR(256) NOT NULL
 );
